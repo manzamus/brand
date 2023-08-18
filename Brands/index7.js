e16a680eadde245f78c1b5cfa5d7fa9eb3130422
mutoni <uwingeneyenazifa29@gmail.com>
2023-08-18 13:37:55 +0200

    brand

---
+++ Brands/index7.js
@@ -0,0 +1,8 @@
+function submit(){
+    const username=document.getElementById("username");
+    const email=document.getElementById("email");
+    const password=document.getElementById("password");
+    const form=document.getElementById("form")
+}
+
+let nazifa=JSON.parse("nazifa")
