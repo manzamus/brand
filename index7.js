cfef013fa0977ca43b9bae22f20052878bcd0d17
mutoni <uwingeneyenazifa29@gmail.com>
2023-08-23 15:36:35 +0200

    brand

--- index7.js
+++ index7.js
@@ -1,131 +1,26 @@
-var emailArray=[];
-var passwordArray=[];
+let username=document.getElementById("username")
+let email=document.getElementById("email")
+let password=document.getElementById("password")
+let form=document.querySelector("form")
 
-var loginBox = document.getElementById("login");
-var regBox = document.getElementById("register");
-var forgetBox = document.getElementById("forgot");
+let users=JSON.parse(localStorage.getItem("users")) || []
 
-var loginTab = document.getElementById("lt");
-var regTab = document.getElementById("rt");
-
-function regTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="visible";
-    loginBox.style.visibility="hidden";
-    forgetBox.style.visibility="hidden";
-
-    regTab.style.backgroundColor="rgb(12, 132, 189)";
-    loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+let user={
+    name:username.value,
+    email:email.value,
+    password:password.value
 }
-function loginTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="hidden";
-    loginBox.style.visibility="visible";
-    forgetBox.style.visibility="hidden";
-
-    loginTab.style.backgroundColor="rgb(12, 132, 189)";
-    regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-}
-function forTabFun(){
-    event.preventDefault();
-
-    regBox.style.visibility="hidden";
-    loginBox.style.visibility="hidden";
-    forgetBox.style.visibility="visible";
-
-    regTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-    loginTab.style.backgroundColor="rgba(11, 177, 224, 0.82)";
-
+if(users.find((userz)=>userz.email==email.value)){
+    alert("user already exist")
+}else{
+    users.push(user);
+    alert("user successfuly created!")
+    const stringify=JSON.stringify(users)
+    localStorage.setItem("users",stringify)
+    console.log(localStorage)
 }
 
 
-function register(){
-    event.preventDefault();
-
-    var email = document.getElementById("re").value;
-    var password = document.getElementById("rp").value;
-    var passwordRetype = document.getElementById("rrp").value;
-
-    if (email == ""){
-        alert("Email required.");
-        return ;
-    }
-    else if (password == ""){
-        alert("Password required.");
-        return ;
-    }
-    else if (passwordRetype == ""){
-        alert("Password required.");
-        return ;
-    }
-    else if ( password != passwordRetype ){
-        alert("Password don't match retype your Password.");
-        return;
-    }
-    else if(emailArray.indexOf(email) == -1){
-        emailArray.push(email);
-        passwordArray.push(password);
-
-        alert(email + "  Thanks for registration. \nTry to login Now");
-
-        document.getElementById("re").value ="";
-        document.getElementById("rp").value="";
-        document.getElementById("rrp").value="";
-    }
-    else{
-        alert(email + " is already register.");
-        return ;
-    }
-}
-function login(){
-    event.preventDefault();
-
-    var email = document.getElementById("se").value;
-    var password = document.getElementById("sp").value;
-
-    var i = emailArray.indexOf(email);
-
-    if(emailArray.indexOf(email) == -1){
-        if (email == ""){
-            alert("Email required.");
-            return ;
-        }
-        alert("Email does not exist.");
-        return ;
-    }
-    else if(passwordArray[i] != password){
-        if (password == ""){
-            alert("Password required.");
-            return ;
-        }
-        alert("Password does not match.");
-        return ;
-    }
-    else {
-        alert(email + " yor are login Now \n welcome to our website.");
-
-        document.getElementById("se").value ="";
-        document.getElementById("sp").value="";
-        return ;
-    }
-
-}
-function forgot(){
-    event.preventDefault();
-
-    var email = document.getElementById("fe").value;
-
-    if(emailArray.indexOf(email) == -1){
-        if (email == ""){
-            alert("Email required.");
-            return ;
-        }
-        alert("Email does not exist.");
-        return ;
-    }
-
-    alert("email is send to your email check it in 24hr. \n Thanks");
-    document.getElementById("fe").value ="";
-}
+})
